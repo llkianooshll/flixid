ddbfc41d715ceb768072f67c4637b513100d07cc
llkianooshll <www.kianoosh002@gmail.com>
2024-10-21 16:19:00 -0700

    responsive

--- components/MoviesCard.tsx
+++ components/MoviesCard.tsx
@@ -3,29 +3,6 @@ import '../src/app/globals.css'
 import { FaPlay } from 'react-icons/fa';
 
 export default function MoviesCard() {
-    const movieImages = ['lonely', 'up'] // یه ارایه ی فیک برای یوزیبل کردن تصاویر بنر
-
-    // استایل های بک گراند بنر ها بصورت متغیر
-    const backgroundStyle1 = {
-        backgroundImage: `url(images/${movieImages[0]}.webp)`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '270px',
-        zIndex: -1, // قرار دادن در پس‌زمینه
-        borderRadius: '18px',
-        boxShadow: '3px 3px 15px black'
-
-    };
-    const backgroundStyle2 = {
-        backgroundImage: `url(images/${movieImages[1]}.webp)`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '270px',
-        zIndex: -1, // قرار دادن در پس‌زمینه
-        borderRadius: '18px',
-        boxShadow: '3px 3px 15px black'
-    };
-
     return (
         // banner component
         <div>
@@ -33,13 +10,13 @@ export default function MoviesCard() {
                 <div className="col-11 col-md-5">
                     <div style={{ border: 'none', background: 'none' }} className="card my-md-5 my-3">
                         <Link href='#' className='a-style'>
-                            <div style={backgroundStyle1} className="card-body">
+                            <div className="card-body banner1">
                                 <div className='row'>
-                                    <div className='col-12 col-md-4 text-white'>
+                                    <div className='col-12 col-lg-4 text-white'>
                                         <div>
                                             <h1>Lonely Girl Movie</h1>
                                         </div>
-                                        <div style={{ marginTop: '40%' }}>
+                                        <div style={{ marginTop: '30%' }}>
                                             <FaPlay style={{ backgroundColor: "black", borderRadius: '50%' }} className='d-inline p-2' size={30} color="#ffffff" />
                                             <b className='mx-2 text-black iran-sans'>پخش فیلم</b>
                                         </div>
@@ -52,13 +29,13 @@ export default function MoviesCard() {
                 <div className="col-11 col-md-7">
                     <div style={{ border: 'none', background: 'none' }} className="card my-md-5 my-3">
                         <Link href="#" className='a-style'>
-                            <div style={backgroundStyle2} className=" card-body">
+                            <div className=" card-body banner2">
                                 <div className='row'>
-                                    <div className='col-12 col-md-3 text-white'>
+                                    <div className='col-12 col-lg-3 text-white'>
                                         <div>
                                             <h1>Up the Movie 2009</h1>
                                         </div>
-                                        <div style={{ marginTop: '40%' }}>
+                                        <div style={{ marginTop: '30%' }}>
                                             <FaPlay style={{ backgroundColor: "black", borderRadius: '50%' }} className='d-inline p-2' size={30} color="#ffffff" />
                                             <b className='mx-2 text-black iran-sans'>پخش فیلم</b>
                                         </div>
